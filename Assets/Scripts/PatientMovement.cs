3ccce8bdcdb6a1c8b4c08c852682a65b48cb7329
Ellen Coutinho <ellencls@al.insper.edu.br>
2025-04-29 07:37:23 -0300

    feat: Adiciona movimentação de Patient
    Prefab de Patient com animações prontas para andar em todas as direções

---
+++ Assets/Scripts/PatientMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PatientMovement : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    public float speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        // Para o personagem começar de frente
+        animator.SetFloat("LastHorizontal", 0f);
+        animator.SetFloat("LastVertical", -1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+        animator.SetBool("IsWalking", horizontal != 0 || vertical != 0);
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetFloat("LastHorizontal", horizontal);
+            animator.SetFloat("LastVertical", vertical);
+        }   
+        Debug.Log($"IsWalking: {animator.GetBool("IsWalking")}, LastVertical: {animator.GetFloat("LastVertical")}, LastHorizontal: {animator.GetFloat("LastHorizontal")}");
+     
+    }
+
+    void FixedUpdate() 
+    {
+        Vector2 movement = new Vector2(
+            animator.GetFloat("Horizontal"),
+            animator.GetFloat("Vertical")
+        ).normalized;
+
+        rb.linearVelocity = movement * speed;
+    }
+}
