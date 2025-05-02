fa4b53d7e66c805018c4966aa61aea8d25eb6d4d
rafaeldbo <rafaeldbo@al.insper.edu.br>
2025-05-02 16:55:10 -0300

    fix: corrigindo bug da porta

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -5,27 +5,30 @@ public class DoorManager : MonoBehaviour {
     private SpriteRenderer spriteRenderer;
     private Animator animator;
     private Collider2D spriteCollider;
+    private bool isOpen = false;
     public bool isLocked;
-    public bool isOpen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
         this.animator = GetComponent<Animator>();
         this.spriteCollider = GetComponent<Collider2D>();
-
-        if (this.isLocked) this.isOpen = false;
     }
 
     // Update is called once per frame
     void Update() {
-        this.animator.SetBool("isOpen", this.isOpen);
-        this.animator.SetBool("isLocked", this.isLocked);
-        this.spriteCollider.enabled = !this.isOpen;
     }
 
     public void Interact() {
         if (!this.isLocked) {
-            this.isOpen = !this.isOpen;
+            AnimatorStateInfo currentAnimState = this.animator.GetCurrentAnimatorStateInfo(0);
+            if (!this.isOpen & currentAnimState.IsName("Idle")) {
+                this.spriteCollider.enabled = false;
+                this.isOpen = true;
+            } else if (this.isOpen & currentAnimState.IsName("DoorOpenFront")) {
+                this.spriteCollider.enabled = true;
+                this.isOpen = false;
+            }
         }
+        this.animator.SetBool("isLocked", this.isLocked);
     }
 }
