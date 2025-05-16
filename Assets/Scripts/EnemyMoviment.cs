d4a296eed278724fc0b0d5dc34eed8bade1eba06
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2025-05-15 22:48:13 -0300

    fix: Criando funções no script EnemyMovement para organizar o código

--- Assets/Scripts/EnemyMoviment.cs
+++ Assets/Scripts/EnemyMoviment.cs
@@ -9,14 +9,83 @@ public class EnemyMoviment : MonoBehaviour
 
     public float atention_level;
     private bool stalk;
+    private Vector2 initialPosition;
+    private string target;
+
     public float speed;
     private Rigidbody2D rb;
     private Animator animator;
     private FieldOfView fieldOfView;
-    private Vector2 initialPosition;
-    private string target;
 
-    string SetFlagsAnimation(string target)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Navigator
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        fieldOfView = GetComponentInChildren<FieldOfView>();
+        audioSource = GetComponent<AudioSource>();
+        initialPosition = rb.position;
+    }
+
+    void FixedUpdate() // Atualização do animator e target
+    {
+        animator.SetBool("Right", false);
+        animator.SetBool("Left", false);
+        animator.SetBool("Up", false);
+        animator.SetBool("Down", false);
+
+        if (stalk) 
+        {
+            animator.SetBool("IsWalking", true);
+            target = "Player";
+        }
+        else if (!stalk && target == "Player")
+        {
+            target = "Inicio";
+        }
+        else if (target == "Nenhum")
+        {
+            animator.SetBool("IsWalking", false);
+            fieldOfView.angleRotation = 0f;
+        }
+        moveToTarget();
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update() // atualização do stalk
+    {
+        if (FieldOfView.canSeePlayer && atention_level < 2) 
+        {
+            atention_level += Time.deltaTime;
+        }
+        else if (!FieldOfView.canSeePlayer && atention_level > 0)
+        {
+            atention_level -= Time.deltaTime;
+        }
+
+        if (atention_level >= 2) 
+        {
+            stalk = true;
+        }
+        if (atention_level <= 0)
+        {
+            stalk = false;
+        }
+
+        playAudio();
+    }
+
+    void moveToTarget()
     {
         Vector2 movement = new Vector2(0, 0);
         if (target == "Player")
@@ -30,9 +99,19 @@ public class EnemyMoviment : MonoBehaviour
             movement = (initialPosition - rb.position).normalized;
             agent.SetDestination(initialPosition);
         }
+
+        if (target == "Inicio" && Mathf.Abs(Vector2.Distance(initialPosition, rb.position)) < 0.1f)
+        {
+            target = "Nenhum";
+        }
+
         float Vx = movement.x;
         float Vy = movement.y;
+        SetFlagsAnimation(Vx, Vy);
+    }
 
+    void SetFlagsAnimation(float Vx, float Vy)
+    {
         if (Mathf.Abs(Vx) > Mathf.Abs(Vy)) 
         {
             if (Vx > 0)
@@ -59,80 +138,9 @@ public class EnemyMoviment : MonoBehaviour
                 fieldOfView.angleRotation = 0f;
             }
         }
-
-        //rb.MovePosition(rb.position + movement*Time.fixedDeltaTime*speed);
-
-        if (target == "Inicio" && Mathf.Abs(Vector2.Distance(initialPosition, rb.position)) < 0.1f)
-        {
-            target = "Nenhum";
-        }
-        return target;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
-
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        fieldOfView = GetComponentInChildren<FieldOfView>();
-        audioSource = GetComponent<AudioSource>();
-        initialPosition = rb.position;
-    }
-
-    void FixedUpdate()
-    {
-        animator.SetBool("Right", false);
-        animator.SetBool("Left", false);
-        animator.SetBool("Up", false);
-        animator.SetBool("Down", false);
-
-        if (stalk) 
-        {
-            animator.SetBool("IsWalking", true);
-            target = "Player";
-        }
-        else if (!stalk && target == "Player")
-        {
-            target = "Inicio";
-        }
-        else if (target == "Nenhum")
-        {
-            animator.SetBool("IsWalking", false);
-            fieldOfView.angleRotation = 0f;
-        }
-        target = SetFlagsAnimation(target);
-    }
-
-    private void LateUpdate()
-    {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (FieldOfView.canSeePlayer && atention_level < 2) 
-        {
-            atention_level += Time.deltaTime;
-        }
-        else if (!FieldOfView.canSeePlayer && atention_level > 0)
-        {
-            atention_level -= Time.deltaTime;
-        }
-
-        if (atention_level >= 2) 
-        {
-            stalk = true;
-        }
-        if (atention_level <= 0)
-        {
-            stalk = false;
-        }
-        
+    void playAudio() {     
         float volumePercent = Mathf.Clamp01(atention_level / maxAttention);
         audioSource.volume = volumePercent;
 
